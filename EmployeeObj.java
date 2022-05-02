e64ee5f52bb758baadf885305cee46a963a0a03c
w9530282 <98965024+w9530282@users.noreply.github.com>
2022-05-02 05:18:23 +0100

    employee obj java file

---
+++ EmployeeObj.java
@@ -0,0 +1,34 @@
+package com.android.smarttest.model;
+
+
+
+public class EmployeeObj {
+
+    String name;
+    String empID;
+    String designation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+}
