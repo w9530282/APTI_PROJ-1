a33db49ff4233a397c5834e8a46f07de5c323c8c
w9530282 <98965024+w9530282@users.noreply.github.com>
2022-05-02 05:17:53 +0100

    candidate java file

---
+++ CandidateObj.java
@@ -0,0 +1,80 @@
+package com.android.smarttest.model;
+
+
+public class CandidateObj {
+
+    public String code;
+    public String name;
+    public String email;
+    public String number;
+    public String technology;
+    public String poc;
+    public String empName;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String result;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+    public String getPoc() {
+        return poc;
+    }
+
+    public void setPoc(String poc) {
+        this.poc = poc;
+    }
+
+}
